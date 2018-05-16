3dd163d2aada27614693059f6dcfa8a340b6bbd3
TaigaMikami <mikamimikami60@gmail.com>
2018-05-16 21:57:38 +0900

    add Home controller

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #top" do
+    it "returns http success" do
+      get :top
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
