1f51acbc7bb28de71574897ec29ee1f4ef68d083
Kyou13 <kyou1013@gmail.com>
2018-05-22 23:08:08 +0900

    rm twikey.py

--- ml/twikey.py
+++
@@ -1,6 +0,0 @@
-twikey={
-"consumer_key" : "8qtcWiBMV7fDxIkvYQ1BfzojW",
-"consumer_secret" : "mBEMIEHxQC1oTD6tLhibAiwUbCSgc06MZFkdWVPsDpnyqEhMJh",
-"access_token" : "514081197-235p20TizwaS8F4taqFmWWhzfxki7kXIAaUrhUgk",
-"access_secret" : "e6SXRAlAaAfrMLremDFZw1dV73E8Px5Ez49aIKEzPfULl"
-        }
