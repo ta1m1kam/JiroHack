d0ffdb45f68ec61875bdc2d8f2c40490fbc81bd8
Kyou13 <kyou1013@gmail.com>
2018-06-09 16:35:02 +0900

    fix ML/main.py

--- ML/main.py
+++ ML/main.py
@@ -22,10 +22,11 @@ from sklearn.model_selection import train_test_split
 app = flask.Flask(__name__)
 CORS(app)
 model = None
+f_model = './model'
 
 def get_model():
     model_path = "./model/vgg16.h5py"
-    global model
+    #global model
     if not os.path.exists(model_path):
         input_tensor = Input(shape=(224,224,3))
         model = VGG16(weights='imagenet',include_top=False,input_tensor=input_tensor)
@@ -36,14 +37,18 @@ def get_model():
         model.save(model_path)
     else:
         model = load_model(model_path)
-    model._make_predict_function()
+    #model._make_predict_function()
+    return model
 
 def prepare_img(img, target_size=(224,224)):
     # img = image.load_img(img,target_size=(224,224))
     img = img.resize(target_size)
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    # x = preprocess_input(x)
+
+    x = x.astype('float32')
+    x = x / 255.0
 
     return x
 
@@ -52,7 +57,7 @@ def prepare_train():
     Y = []
 
     # 対象Aの画像
-    for picture in list_pictures('./ramen/'):
+    for picture in list_pictures('./ordinary/'):
         img = img_to_array(load_img(picture, target_size=(224,224)))
         X.append(img)
 
@@ -60,7 +65,7 @@ def prepare_train():
 
 
     # 対象Bの画像
-    for picture in list_pictures('./ラーメン二郎/'):
+    for picture in list_pictures('./jiro/'):
         img = img_to_array(load_img(picture, target_size=(224,224)))
         X.append(img)
 
@@ -84,19 +89,46 @@ def prepare_train():
 @app.route('/predict', methods=["POST"])
 def predict():
     data = {"success": False}
+    # global model
+    ## API 部分
+    from keras.models import model_from_json
+    model = model_from_json(open("model/model.json").read())
+    model.load_weights('model.ep10.h5py')
+    print(type(model))
+    ## Rails
     enc_data  = flask.request.form['img']
     dec_data = base64.b64decode( enc_data.split(',')[1] )
-
     # image = flask.request.files["image"].read()
     image = Image.open(io.BytesIO(dec_data)).convert("RGB")
     image = prepare_img(image)
-
     preds = model.predict(image)
-    results = decode_predictions(preds)
-    data["predictions"] = []
 
-    for (imagenetID, label, prob) in results[0]:
-        r = {"label": label, "probability": float(prob)}
+    ## Local
+    ## if flask.request.method == "POST":
+    ##     if flask.request.files.get("image"):
+    ##         image = flask.request.files["image"].read()
+    ##         image = Image.open(io.BytesIO(image))
+    ##         image = prepare_img(image)
+    ##         import pdb;pdb.set_trace()
+
+    ##         preds = model.predict(image)
+    ##         # results = decode_predictions(preds)
+    ##         data["predictions"] = []
+
+
+    ##         for label,i in enumerate(preds[0]):
+    ##            r = {"label": label, "probability": float(i)}
+    ##            data["predictions"].append(r)
+    ##            data["success"] = True
+
+    # for (imagenetID, label, prob) in results[0]:
+    #     r = {"label": label, "probability": float(prob)}
+    #     data["predictions"].append(r)
+    #     data["success"] = True
+    data["predictions"] = []
+    for label,i in enumerate(preds[0]):
+        score = str(int(float(i) * 500))[:-1] + "0"
+        r = {"label": label, "probability": score}
         data["predictions"].append(r)
         data["success"] = True
     print(data)
@@ -104,9 +136,13 @@ def predict():
 
 def create_last_conv2d_fine_tuning(classes):
     # vgg16モデルを作る
-    get_model()
-    global model
-    vgg16_model = model
+    vgg16_model = get_model()
+    # global model
+    # vgg16_model = model
+    input_tensor = Input(shape=(224,224,3))
+
+    for layer in vgg16_model.layers:
+        layer.trainable = False
 
     x = vgg16_model.output
     x = Flatten()(x)
@@ -116,8 +152,8 @@ def create_last_conv2d_fine_tuning(classes):
     predictions = Dense(classes, activation='softmax')(x)
     model = Model(inputs=vgg16_model.input, outputs=predictions)
     # 最後の畳み込み層より前の層の再学習を防止
-    for layer in model.layers[:15]:
-        layer.trainable = False
+    # for layer in model.layers[:15]:
+    #     layer.trainable = False
 
     model.compile(loss='categorical_crossentropy',
                  optimizer=SGD(lr=1e-4, momentum=0.9),
@@ -126,29 +162,59 @@ def create_last_conv2d_fine_tuning(classes):
 
 if __name__ == "__main__":
     print("Loading Keras model and Flask starting server...\n")
-    # get_model()
-    # app.run(host='0.0.0.0', port=80)
+    #get_model()
+    app.run(host='0.0.0.0', port=80)
+    # app.run()
 
-    #-- TRAINING MODEL --#
-    model = create_last_conv2d_fine_tuning(2)
-    # 実行。出力はなしで設定(verbose=0)。
-    X_train, X_test, y_train, y_test = prepare_train()
-    callbacks = []
-    callbacks.append(EarlyStopping("val_loss", patience=1))
-    callbacks.append(ModelCheckpoint(filepath="model.ep{epoch:02d}.h5",monitor='val_loss'))
-    history = model.fit(X_train, y_train, batch_size=5, epochs=20,
-                    validation_data = (X_test, y_test), verbose = 1,callbacks=callbacks)
+#-- TRAINING --#
+    # model = create_last_conv2d_fine_tuning(2)
+    # ## # 実行。出力はなしで設定(verbose=0)。
+    # X_train, X_test, y_train, y_test = prepare_train()
+    # callbacks = []
+    # callbacks.append(EarlyStopping("val_loss", patience=1))
+    # callbacks.append(ModelCheckpoint(filepath="model.ep{epoch:02d}.h5py"))
+    # history = model.fit(X_train, y_train, batch_size=32, epochs=10,\
+    #                 validation_data=(X_test, y_test), verbose=1, callbacks=callbacks)
+    # score = model.evaluate(X_test, y_test, verbose=0)
+    # json_string = model.to_json()
+    # open(os.path.join(f_model,'model.json'), 'w').write(json_string)
+    # print('Test loss:', score[0])
+    # print('Test accuracy:', score[1])
+
+    # # Prediction
+    # import numpy as np
+    # from sklearn.metrics import confusion_matrix
+
+    # predict_classes = model.predict_classes(X_test[1:10,], batch_size=32)
+    # true_classes = np.argmax(y_test[1:10],1)
+    # print(confusion_matrix(true_classes, predict_classes))
 
 
 
+# Predict
     # model = get_model()
     # model.summary()
 
-    # filename = sys.argv[1]
-    # img = image.load_img(filename,target_size=(224,224))
-    # x = image.img_to_array(img)
-    # x = np.expand_dims(x, axis=0)
+    ## model.load_weights('model.ep04.h5py')
+    ## filename = sys.argv[1]
+    ## img = image.load_img(filename,target_size=(224,224))
+    ## x = image.img_to_array(img)
+    ## x = np.expand_dims(x, axis=0)
+    ## result = model.predict(x)
+    ## print(result)
     # preds = model.predict(preprocess_input(x))
-    # results = decode_predictions(preds, top=5)[0]
+    ## results = decode_preditions(preds, top=5)[0]
     # for result in results:
     #     print(result)
+    ## from keras.models import model_from_json
+    ## model = model_from_json(open("model/model.json").read())
+    ## model.load_weights('model.ep10.h5py')
+    ## filename = sys.argv[1]
+    ## img = image.load_img(filename,target_size=(224,224))
+    ## x = image.img_to_array(img)
+    ## x = np.expand_dims(x, axis=0)
+    ## x = x.astype('float32')
+    ## x = x / 255.0
+
+    ## result = model.predict(x)
+    ## print(result)
